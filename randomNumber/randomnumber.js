d5efa82c1cb192937b896d537b81ff0ef2ae8b18
Paul Borawski <paulborawski14@gmail.com>
2016-03-27 07:43:00 -0700

    random number generator

---
+++ randomNumber/randomnumber.js
@@ -0,0 +1,21 @@
+// Example
+function ourFunction(ourMin, ourMax) {
+
+  return Math.floor(Math.random() * (ourMax - ourMin + 1)) + ourMin;
+}
+
+ourFunction(1, 9);
+
+// Only change code below this line.
+
+function randomRange(myMin, myMax) {
+
+  return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin; // Change this line
+
+}
+
+// Change these values to test your function
+var myRandom = randomRange(5, 15);
+console.log(myRandom);
+console.log(randomRange(14, 28));
+console.log(randomRange(4, 110));
---
+++ randomNumber/readme.markdown
@@ -0,0 +1,2 @@
+#random number generator
+It will give you a random number between any two numbers that you plug in as parameters
