538cf99822e072883b4b35f077ed90fa3b657ad2
Paul Borawski <paulborawski14@gmail.com>
2016-04-16 21:47:44 -0700

    fixed quote machine

--- quotes/js.js
+++ quotes/js.js
@@ -1,25 +1,19 @@
-var quotes = new Array();
-quotes[0] = "'If you're not first, you're last' -Ricky Bobby";
-quotes[1] = "'Nothing Satisfies' -Bent Cardan";
-quotes[2] = "'Pretty good' -Larry David";
-quotes[3] = "'See you tomorrow' -Wils Cardan";
-quotes[4] = "'I am Nawa!' -Paul Borawski";
-quotes[5] = "'Sometimes life hits you in the head with a brick. Don't lose faith.' -Steve Jobs";
-quotes[6] = "'The only limit to our realization of tomorrow will be our doubts of today.' -Franklin D. Roosevelt";
-quotes[7] = "'What would life be if we had no courage to attempt anything?' -Vincent Van Gogh";
-quotes[8] = "'By failing to prepare, you are preparing to fail.' -Benjamin Franklin";
-quotes[9] = "'Spring is nature's way of saying, 'Let's party!' -Robin Williams";
-quotes[10] = "'Genius is patience.' -Isaac Newton";
-
-
+var quotes = [
+  "'If you're not first, you're last' -Ricky Bobby",
+  "'Nothing Satisfies' -Bent Cardan",
+  "'Pretty good' -Larry David",
+  "'See you tomorrow' -Wils Cardan",
+  "'I am Nawa!' -Paul Borawski",
+  "'Sometimes life hits you in the head with a brick. Don't lose faith.' -Steve Jobs",
+  "'The only limit to our realization of tomorrow will be our doubts of today.' -Franklin D. Roosevelt",
+  "'What would life be if we had no courage to attempt anything?' -Vincent Van Gogh",
+  "'By failing to prepare, you are preparing to fail.' -Benjamin Franklin",
+  "'Spring is nature's way of saying, 'Let's party!' -Robin Williams",
+  "'Genius is patience.' -Isaac Newton"
+]
 
 function printQuote() {
-  var rando = Math.floor(Math.random() * 11);
-  var quote = document.getElementById("emptyone");
-  quote.innerHTML = quotes[rando];
-}
-
-function printQuote2() {
-  var quote2 = document.getElementById("emptytwo");
-  quote2.innerHTML = quotes[rando];
+  document
+    .getElementById("emptyone")
+    .innerHTML = quotes[Math.floor(Math.random() * 11)];
 }
